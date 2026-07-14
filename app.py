650c51aab4fca85e0e71053f3c72ab9f810021f5
Jayme Leita <theleitas@Jaymes-MacBook-Pro.local>
2026-07-13 22:37:20 -0400

    Add Masters to tournament history

--- app.py
+++ app.py
@@ -204,6 +204,14 @@ PGATOUR_FIELD_URLS_BY_ESPN_EVENT_ID = {
 }
 HISTORY_RESULTS = [
     {
+        "tournament": "2026 Masters",
+        "placements": {
+            "Spencer Tidwell": "1st",
+            "Jayme Leita": "2nd",
+            "Peter Miller": "3rd",
+        },
+    },
+    {
         "tournament": "2026 U.S. Open",
         "placements": {
             "Jayme Leita": "1st",
